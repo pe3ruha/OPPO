3e5dce8f6567a0711c188e322f8a190236b868c7
pe3ruha <pe3ruha96@mail.ru>
2025-12-14 15:38:45 +0700

    1

--- ReadSalinity.cpp
+++ ReadSalinity.cpp
@@ -4,13 +4,13 @@
 bool ReadSalinity(const string& line, const regex& salRegex, Sea& temp, int lineNumber) {
     smatch match;
     if (!regex_search(line, match, salRegex)) {
-        cout << "Ошибка: Солёность не найдена. Строка " << lineNumber + 1 << endl;
+        cout << "Ошибка: Солёность не найдена. Строка: " << lineNumber + 1 << endl;
         return false;
     }
 
     float value = stof(match.str(0));
     if (value < 0 || value > 100) {
-        cout << "Ошибка: Солёность 0–100. Строка " << lineNumber + 1 << endl;
+        cout << "Ошибка: Солёность 0–100. Строка: " << lineNumber + 1 << endl;
         return false;
     }
 
